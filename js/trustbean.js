74ac3c53c255105503f6f0b50365ef6348230667
W Arun Taluker <bdarun7@gmail.com>
2017-04-28 01:17:54 +0600

    almost Done

--- js/trustbean.js
+++ js/trustbean.js
@@ -1,8 +1,13 @@
-var  firstname=getElementById('#firstName');
-var lastname=getElementById('#lastName');
-var password=getElementById('#password');
-var email=getElementById('#emailAddress');
-var  submit=getElementById('#sendEmail');
+var  firstname=getElementById('firstName');
+var lastname=getElementById('lastName');
+var password=getElementById('password');
+var email=getElementById('emailAddress');
+var  submit=getElementById('sendEmail');
 function firstName(){
-	if()
+	if(firstname.length!=0 && firstname.length<=50){
+		document.write.getElementById('name').innerText= firstname+"  "+lastname;
+	}else
+	{ 
+		document.write.getElementById('name').innerText="Invalate input";
+	}
 }
